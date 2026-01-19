2ed6dfc856a37a5dd3eecbca047bdcc2b8a2cfa6
icarus <eurfelux@gmail.com>
2026-01-19 21:38:45 +0800

    refactor: Remove App.css and add padding to TurtlePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import TurtlePage from "./pages/turtle-page";
 
 function App() {
--- src/pages/turtle-page.tsx
+++ src/pages/turtle-page.tsx
@@ -1,6 +1,6 @@
 const TurtlePage = () => {
 	return (
-		<div>
+		<div className="p-8">
 			<h1>Turtle Game</h1>
 			<p>Welcome to the Turtle Game!</p>
 		</div>
