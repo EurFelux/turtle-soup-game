999d433944db84cc33c1cb040ca848859f123447
icarus <eurfelux@gmail.com>
2026-01-20 17:25:43 +0800

    feat: Add transition effect to soup item hover state

--- src/pages/turtle-page/soup-item.tsx
+++ src/pages/turtle-page/soup-item.tsx
@@ -11,7 +11,7 @@ const SoupItem = ({ soup, active, onClick }: SoupItemProps) => {
 	return (
 		<li
 			className={cn(
-				"h-10 w-full cursor-pointer truncate rounded-lg p-2",
+				"h-10 w-full cursor-pointer truncate rounded-lg p-2 transition-colors",
 				"hover:bg-secondary-hover",
 				active && "bg-primary text-primary-foreground hover:bg-primary-hover",
 			)}
