aea83cb1798dff97e16d6cc322d1d5c9f0256c33
icarus <eurfelux@gmail.com>
2026-01-23 05:22:57 +0800

    fix: Show warning when AI settings are invalid on turtle page

--- src/pages/turtle-page/no-active-soup.tsx
+++ src/pages/turtle-page/no-active-soup.tsx
@@ -1,4 +1,7 @@
+import { TriangleAlertIcon } from "lucide-react";
 import { useTranslation } from "react-i18next";
+import { checkAiSettings } from "@/business/ai";
+import { Alert } from "@/components/ui/alert";
 import type { AiSettings } from "@/types";
 import CreateSoupContent from "./create-soup-content";
 
@@ -8,6 +11,7 @@ type NoActiveSoupProps = {
 
 const NoActiveSoup = ({ aiSettings }: NoActiveSoupProps) => {
 	const { t } = useTranslation();
+	const isValidAiSettings = checkAiSettings(aiSettings);
 	return (
 		<div className="flex h-full flex-col items-center justify-center rounded-lg bg-secondary">
 			<div className="flex h-1/3 w-1/2 flex-col gap-4">
@@ -17,6 +21,13 @@ const NoActiveSoup = ({ aiSettings }: NoActiveSoupProps) => {
 					</h2>
 					<span>{t("page.turtle.create_soup.description")}</span>
 				</div>
+
+				{!isValidAiSettings && (
+					<Alert className="my-2 text-warning">
+						<TriangleAlertIcon className="size-4" />
+						{t("page.turtle.error.invalid_ai_settings")}
+					</Alert>
+				)}
 
 				<CreateSoupContent aiSettings={aiSettings} className="flex-1" />
 			</div>
